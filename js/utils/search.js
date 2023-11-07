486228e331ef0583ec4220ed5877c7ee7ac30fbe
RyanDjafri <ryandjafripro@gmail.com>
2023-11-07 10:57:21 +0100

    search-bar 50M

---
+++ js/utils/search.js
@@ -0,0 +1,27 @@
+const searchBar = document.getElementById("meal");
+const searchIcon = document.querySelector(".search-icon-div");
+const icon = document.querySelector(".search-icon");
+const originalSrc = icon.src;
+
+searchIcon.addEventListener("mouseover", () => {
+  icon.src = "../../assets/icons/search-hover.svg";
+});
+
+searchIcon.addEventListener("mouseout", () => {
+  icon.src = originalSrc;
+});
+
+searchBar.addEventListener("input", () => {
+  const cancel = document.querySelector(".cancel");
+  if (searchBar.value.length > 1) {
+    cancel.style.display = "block";
+  } else {
+    cancel.style.display = "none";
+  }
+  if (cancel.style.display === "block") {
+    cancel.addEventListener("click", () => {
+      searchBar.value = "";
+      cancel.style.display = "none";
+    });
+  }
+});
